f90d775be2e3983525839aeb8fced4e209281a78
djh82uk <djh82uk@gmail.com>
2021-06-20 00:18:40 +0100

    Changed to PIO for CLK gen

--- siggen.cpp
+++ siggen.cpp
@@ -7,16 +7,24 @@
 #include "GFX.hpp"
 #include "pico/multicore.h"
 #include <string> 
+#include "hardware/pio.h"
+#include "hardware/clocks.h"
+#include "clk.pio.h"
 using namespace std;
 
 
-int freq = 1000;
-float freqhz;
+//Declare Variables
 string mode = "auto";
+double freq = 1.00;
+uint32_t base = 62500000;
+uint sm;
+PIO pio = pio0;
+uint32_t freqcalc = base / freq; // 62500000 / freq in hz
+float btn_time;
 
 // Debounce control
 unsigned long time = to_ms_since_boot(get_absolute_time());
-const int delayTime = 100; // 50ms worked fine for me .... change it to your needs/specs.
+const int delayTime = 200; // 50ms worked fine for me .... change it to your needs/specs.
 
 
 static int core0_rx_val = 0, core1_rx_val = 0;
@@ -39,86 +47,103 @@ void core1_sio_irq() {
     multicore_fifo_clear_irq();
 }
 
+void clk_gen(PIO pio, uint sm, uint offset, uint pin, uint freq);
 
 
 
+// Interupt Function
 void freq_change_irq_handler(uint gpio, uint32_t events) {
         if ((to_ms_since_boot(get_absolute_time())-time)>delayTime) {
             time = to_ms_since_boot(get_absolute_time());           
             
-            if (gpio == 14 && (freq > 1)) {
-                printf("Frequency Increase\n");
-                while (gpio_get(gpio) == 1) {
-                    if (freq > 100) {
-                    printf("Sustained Up\n");
-                    freq = freq - 10;
-                    sleep_ms(100);
-                    } else if (freq <= 100 && ( freq > 1)) {
-                    printf("Throttled Up\n");
-                    freq = freq - 1;
-                    sleep_ms(100);
-
-                    } else {
-                    printf("Upper Limit Reached\n");
+            if (gpio == 15 && (freq < 50000.0)) { //Max Frequency is 50KHz
+                printf("Frequency Increase\n");  
+                while (gpio_get(gpio) == 1 && (freq <= 50000.0)) {  
+                    if (freq <= 50000.0) {
+                    printf("Time = %i\n" ,((to_ms_since_boot(get_absolute_time())-time)));
+                    btn_time = ((to_ms_since_boot(get_absolute_time())-time));
+                    if (btn_time > 1000) {
+                        btn_time = 1000;
+                    } 
+                    else if (btn_time == 0) {
+                        freq = freq + 1;
                     }
+                    sleep_ms(500);
+                    freq = freq + btn_time; //Scale the increment by how long the button has bee held
+                    if (freq > 50000.0) {
+                        freq = 50000;
+                    }
+                    sleep_ms(100);
+                    freqcalc = base / freq; // 62500000 / freq in hz
+                    } 
                 
                 }
         }
-            else if (gpio == 15 && (freq <5000)) {
+    
+            else if (gpio == 14 && (freq > 0.2)) {
                 printf("Frequency Decrease\n");
-                while (gpio_get(gpio) == 1) {
-                    if (freq < 5000) {
-                    printf("Sustained Down\n");
-                    freq = freq + 10;
-                    sleep_ms(100);
-                    } else {
-                    printf("Lower Limit Reached\n");
+                while (gpio_get(gpio) == 1 && (freq > 0.2)) {
+                    if (freq >= 0.2) {
+                    printf("Time = %i\n" ,((to_ms_since_boot(get_absolute_time())-time)));
+                    btn_time = ((to_ms_since_boot(get_absolute_time())-time));
+                    if (btn_time > 1000) {
+                    btn_time = 1000;
+                    } 
+                    else if (btn_time == 0) {
+                        freq = freq - 1;  //Scale the Deccrement by how long the button has bee held
                     }
+                    sleep_ms(500);
+                    freq = freq - btn_time;
+                    if (freq < 0.2) {
+                        freq = 0.2;
+                    }
+                    sleep_ms(100);
+                    freqcalc = base / freq; // 62500000 / freq in hz
                 
-                }
+                    } 
+
+                }           
+        
                 
-                
-                } else if (gpio == 12) {
+                } else if (gpio == 12) { // Detect if Mode Button is Pressed
                     printf("Mode Toggle\n");
                     if (mode == "auto") {
                         mode = "manual";
+                        pio_sm_set_enabled(pio, sm, false);  //Disable the PIO for Manual Mode
                     }
                     else if (mode == "manual") {
                         mode = "auto";
+                        pio_sm_set_enabled(pio, sm, true);  //Enable the PIO for Auto Mode
                     }
                     }
                     
                 else if (gpio == 11 && (mode == "manual")) {
                     printf("Manual Pulse\n");
-                    gpio_put(13, 1);
+                    //Need to fill in and figure out how to pulse the CLK when it is handled by the PIO
                     sleep_ms(500);
-                    gpio_put(13, 0);
-                    sleep_ms(500);
-                    }
-            
+                }
+                    }          
             
         
     
+    
+    // Clear the Buffer and Re-initialise
+    pio_sm_clear_fifos(pio, sm);
+    pio->txf[sm] = freqcalc;
 
-        printf("Interupt on Pin: %i\n", gpio);
 
-}    
 }
-
-
 
 // Core 1 Main Code
 void core1_entry() {
     printf("Starting Core 1\n");
-    const uint LED_PIN = PICO_DEFAULT_LED_PIN;
-    gpio_init(LED_PIN);
-    gpio_set_dir(LED_PIN, GPIO_OUT);
+
+    
     multicore_fifo_clear_irq();
     irq_set_exclusive_handler(SIO_IRQ_PROC1, core1_sio_irq);
-
     irq_set_enabled(SIO_IRQ_PROC1, true);
-    gpio_init(13);
-    gpio_set_dir(13, GPIO_OUT);
+    
+    //Setup Interupt Pins
     gpio_init(11);
     gpio_pull_down(11);
     gpio_init(12);
@@ -132,33 +157,31 @@ void core1_entry() {
     gpio_set_irq_enabled_with_callback(12, GPIO_IRQ_EDGE_RISE  , true, &freq_change_irq_handler);
     gpio_set_irq_enabled_with_callback(11, GPIO_IRQ_EDGE_RISE  , true, &freq_change_irq_handler);
     
+    while(true) 
+    {
+        // Keep the Buffer Filled
+        sleep_us(10);
+        freqcalc = base / freq; // 62500000 / freq in hz
+        pio->txf[sm] = freqcalc; 
+    }
 
-
-    // Send something to Core0, this should fire the interrupt.
-    
-
-    while(true) {
-        if (mode == "auto") {
-            gpio_put(13, 1);
-            sleep_ms(freq / 2);
-            gpio_put(13, 0);
-            sleep_ms(freq / 2);
-            multicore_fifo_push_blocking(freq);
-    } 
-    } 
-    
 
 }
 
+
+
+
+
 int main() {
     stdio_init_all();
-    
- 
-    multicore_launch_core1(core1_entry);
+    sleep_ms(1000);
+    uint offset = pio_add_program(pio, &clk_program);
+    printf("Loaded program at %d\n", offset);
+    clk_gen(pio, 0, offset, 13, freq);
+
     irq_set_exclusive_handler(SIO_IRQ_PROC0, core0_sio_irq);
     irq_set_enabled(SIO_IRQ_PROC0, true);
     //setup
-    //stdio_init_all();
     i2c_init(i2c1, 400000);                 //Initialize I2C on i2c1 port with 400kHz
     gpio_set_function(2, GPIO_FUNC_I2C);    //Use GPIO2 as I2C
     gpio_set_function(3, GPIO_FUNC_I2C);    //Use GPIO3 as I2C
@@ -174,17 +197,23 @@ int main() {
     {
         sleep_ms(1000);
         oled.clear();                       //Clear buffer
-        freqhz = 1.0 / freq;
-        freqhz = freqhz * 1000;
         string ssfreq = to_string(freq);
-        string ssfreqhz = to_string(freqhz);
-        ssfreqhz.erase(ssfreqhz.size() - 4);
+        ssfreq.erase(ssfreq.size() - 4);
         oled.drawString(0, 0,  "mode: " + mode);
-        oled.drawString(0, 10,  "Period: " + ssfreq);
-        oled.drawString(0, 20,  "Frequency: " + ssfreqhz + "Hz");
-        
-       
-        
+        if (freq < 1000.0) {
+            string ssfreq = to_string(freq);
+            ssfreq.erase(ssfreq.size() - 4);
+            oled.drawString(0, 10,  "Frequency: " + ssfreq + "Hz");
+        } else if (freq < 1000000.0) {
+            string ssfreq = to_string(freq / 1000);
+            ssfreq.erase(ssfreq.size() - 4);
+            oled.drawString(0, 10,  "Frequency: " + ssfreq + "KHz");
+        } else {
+            string ssfreq = to_string(freq / 1000000);
+            ssfreq.erase(ssfreq.size() - 4);
+            oled.drawString(0, 10,  "Frequency: " + ssfreq + "MHz");
+        } 
+     
 
         oled.display();                     //Send buffer to the screen
         //printf("Irq handlers should have rx'd some stuff - core 0 got %d, core 1 got %d!\n", core0_rx_val, core1_rx_val);
@@ -193,5 +222,10 @@ int main() {
 }
 
 
+void clk_gen(PIO pio, uint sm, uint offset, uint pin, uint freq) {
+    clk_program_init(pio, sm, offset, pin);
+    pio_sm_set_enabled(pio, sm, true);
+    multicore_launch_core1(core1_entry);
+}
 
 
